0a37f56fbca94387deac8c3f4a701b124bdc8dc9
Josh Sacks <josh.sacks@gmail.com>
2024-06-11 15:56:58 -0700

    working serving static NextJS

--- server/app.py
+++ server/app.py
@@ -1,9 +1,18 @@
+import logging
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
+
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    logging.info("Here1")
+    return app.send_static_file('index.html')
+
+@app.route('/foo')
+def foo():
+
+    return "hello, world"
 
 if __name__ == '__main__':
     app.run(debug=True)
