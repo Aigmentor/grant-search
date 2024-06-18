9e90ebfed5f4d56bb06df8fac9aa6bf7fdd00ebd
Josh Sacks <josh.sacks@gmail.com>
2024-06-18 16:31:24 -0600

    Refactor API into separate Flask file

--- server/app.py
+++ server/app.py
@@ -1,30 +1,26 @@
 import logging
 import os
-from flask import Flask, jsonify, request, session, redirect, url_for
+from flask import Flask, request, session, redirect, url_for
 from server.common import MODE_ENUM, get_mode
 from server.db.database import init_db
 import server.db.models as db
 import server.oauth as oauth
-import server.db.database as database
+import server.web_api as web_api
+logging.basicConfig(level=logging.DEBUG)
 
 if get_mode() == MODE_ENUM.LOCAL:
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_folder='../static')
 app.secret_key = os.environ['FLASK_SECRET_KEY']
-logging.basicConfig(level=logging.DEBUG)
+
+app.register_blueprint(web_api.api, url_prefix='/api')
 
 init_db()
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
-@app.route("/api/auth")
-def auth():
-    credentials = oauth.get_credentials_from_flask_session()
-    is_logged_in = credentials is not None and not credentials.expired
-    return jsonify({'isLoggedIn': is_logged_in})
 
 @app.route("/app/login/<string:state>")
 def login(state: str):
@@ -43,14 +39,6 @@ def handle_login():
 
     return redirect(url_for('index', status=status))
 
-@app.route("/api/status")
-def handle_status():
-    credentials = oauth.get_credentials_from_flask_session()
-    is_logged_in = credentials is not None and not credentials.expired
-    if not is_logged_in:
-        return jsonify({'error': 'Not logged in'}), 400
-    user = db.GoogleUser.get_or_create(session.get('username'), oauth.serialize_credentials(credentials))       
-    return jsonify({'status': user.status.status, 'email': user.email})
 
 if __name__ == '__main__':
     app.run(debug=True)
