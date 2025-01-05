dbe0dd671754cdc8a734984581a4d6441e56dc09
Josh Sacks <josh.sacks@gmail.com>
2025-01-05 14:19:49 -0700

    Use ingester to update grants

--- grant_search/ingest/update.py
+++ grant_search/ingest/update.py
@@ -10,14 +10,10 @@ from pydantic import BaseModel, Field
 
 from grant_search.db.database import get_session
 from grant_search.db.models import Grant, GrantDerivedData
-from grant_search.ai.common import format_for_llm, get_ai_client
+from grant_search.ingest.send_to_ai import SendToAI
 
 MAX_CONCURRENT_GRANTS = 100
-MODEL = "gpt-4o"
 
-PROMPT = """
-Process the grant description below to generate a succinct summary of the grant's topic.
-"""
 
 # Set up logging to stdout
 logging.basicConfig(
@@ -27,42 +23,31 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-class UpdateModel(BaseModel):
-    summary: str = Field(
-        description="A succinct (80-120 tokens) summary of the grant's topic and what they hope to achieve.",
-    )
+ai_processor = SendToAI()
 
 
 # Update this function to check if the grant needs to be updated
 def needs_update(grant: Grant):
-    # return True
-    return grant.derived_data.summary is None
-
-
-# Modify this function to update the grant with the model-derived data
-def update_grant(grant: Grant, results: UpdateModel):
-    # logging.info(
-    #     f"Updating grant {grant.id} \nx{grant.description} \n\n with summary: {results}"
-    # )
-    grant.derived_data.summary = results.summary
+    return grant.derived_data is None or grant.derived_data.summary is None
 
 
 def process_grant(grant_id: int):
     with get_session() as process_session:
         grant = process_session.get(Grant, grant_id)
-        if grant:
-            messages = format_for_llm(PROMPT, grant.description)
-            results = get_ai_client().chat.completions.create(
-                model=MODEL,
-                messages=messages,
-                response_model=UpdateModel,
-                max_tokens=300,
+        _, analysis = ai_processor.process_single_grant(grant)
+        derived_data = grant.derived_data
+        if derived_data is None:
+            logger.info(f"Creating derived data for grant {grant.id}")
+            derived_data = GrantDerivedData(
+                grant_id=grant.id,
+                **analysis.model_dump(),
             )
-            update_grant(grant, results)
-            process_session.commit()
+            process_session.add(derived_data)
         else:
-            print(f"Grant {grant_id} not found")
+            for field, value in analysis.model_dump().items():
+                setattr(derived_data, field, value)
+
+        process_session.commit()
     return grant_id
 
 
